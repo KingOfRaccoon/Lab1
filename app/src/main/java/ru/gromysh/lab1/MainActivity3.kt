e358caf74a0c5ac9170119b399d4ba23866c5c4e
KingOfRaccoon <zvezdakov2015@yandex.ru>
2024-04-07 06:28:53 +0300

    update to lab 2

--- app/src/main/java/ru/gromysh/lab1/MainActivity3.kt
+++ app/src/main/java/ru/gromysh/lab1/MainActivity3.kt
@@ -1,7 +1,10 @@
 package ru.gromysh.lab1
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,5 +15,11 @@ class MainActivity3: AppCompatActivity() {
 
         val fio = intent.getStringExtra(tagName)
         findViewById<TextView>(R.id.text1).text = "Добро пожаловать, $fio"
+
+        val button = findViewById<Button>(R.id.button2)
+        button.setOnClickListener {
+            startActivity(Intent(this, MainActivity4::class.java))
+            finish()
+        }
     }
 }
